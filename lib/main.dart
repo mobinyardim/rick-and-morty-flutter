0ddcc9dce01ce2b825868d3d640962d00079db9c
MobinYardim <mobinyardim@gmail.com>
2023-04-17 04:11:09 +0330

    add router config to app widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'Pages/main_page.dart';
+import 'package:rick_and_morty_flutter/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-final routes = GoRouter(routes: [
-  GoRoute(
-      path: "/",
-      builder: (context, state) {
-        return const MainPage(title: "Rick And Morty");
-      })
-]);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -28,7 +20,7 @@ class MyApp extends StatelessWidget {
         ),
         primarySwatch: Colors.blue,
       ),
-      routerConfig: routes,
+      routerConfig: routerConfig,
     );
   }
 }
