c11898f93f5c4e57167583afe6364402e9791c11
MobinYardim <mobinyardim@gmail.com>
2023-04-17 04:10:50 +0330

    create router config

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,3 +1,11 @@
+import 'package:go_router/go_router.dart';
+
+import '../Pages/characters/characters_page.dart';
+import '../Pages/episodes/episodes_page.dart';
+import '../Pages/home/home_page.dart';
+import '../Pages/locations/locations_page.dart';
+import '../Pages/main_page.dart';
+
 enum Routes {
   home(path: "/"),
   characters(path: "/characters"),
@@ -8,3 +16,32 @@ enum Routes {
 
   final String path;
 }
+
+final routerConfig = GoRouter(routes: [
+  ShellRoute(
+      builder: (context, state, child) {
+        return MainPage(title: "Rick And Morty", child: child);
+      },
+      routes: [
+        GoRoute(
+            path: Routes.home.path,
+            builder: (context, state) {
+              return const HomePage();
+            }),
+        GoRoute(
+            path: Routes.characters.path,
+            builder: (context, state) {
+              return const CharactersPage();
+            }),
+        GoRoute(
+            path: Routes.episodes.path,
+            builder: (context, state) {
+              return const EpisodesPage();
+            }),
+        GoRoute(
+            path: Routes.locations.path,
+            builder: (context, state) {
+              return const LocationsPage();
+            })
+      ])
+]);
