272932411236b4ea3fe3f955cfb47e6fffa235c6
MobinYardim <mobinyardim@gmail.com>
2023-04-21 20:05:56 +0330

    add grid to home page

--- lib/Pages/home/home_page.dart
+++ lib/Pages/home/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty_flutter/components/character_item.dart';
 
+import '../../utils/character_faker.dart';
+import '../../utils/window_utils.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -11,11 +14,18 @@ class HomePage extends StatefulWidget {
 class _HomePAgeState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        CharacterItem()
-      ],
-    );
+    double itemWidth = isDesktop(context) ? 400 : 200;
+    double ratio = isDesktop(context) ? 2.3 : 0.56;
+
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) =>
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: itemWidth, childAspectRatio: ratio),
+              itemBuilder: (item, index) => (Align(
+                  child:
+                      CharacterItem(character: fakeNullCharacterList[index]))),
+              itemCount: fakeNullCharacterList.length,
+            ));
   }
 }
