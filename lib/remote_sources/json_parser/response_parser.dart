62f0ccae3e3ee2e11ef7417e5407f3ef4375f8e3
MobinYardim <mobinyardim@gmail.com>
2023-04-24 20:59:27 +0330

    log exception

--- lib/remote_sources/json_parser/response_parser.dart
+++ lib/remote_sources/json_parser/response_parser.dart
@@ -1,5 +1,6 @@
 import 'package:rick_and_morty_flutter/remote_sources/dto/response_dto.dart';
 import 'package:rick_and_morty_flutter/remote_sources/json_parser/base_json_parser.dart';
+import '../../utils/logger.dart';
 
 class ResponseParser<T> extends BaseJsonParser<ResponseDto> {
   final BaseJsonParser<T> resultParser;
@@ -7,9 +8,9 @@ class ResponseParser<T> extends BaseJsonParser<ResponseDto> {
   const ResponseParser({required this.resultParser});
 
   @override
-  ResponseDto fromJson(Map<String, dynamic> json) {
+  ResponseDto<T> fromJson(Map<String, dynamic> json) {
     try {
-      var results = (json["results"] as List<Map<String, dynamic>>);
+      var results = (json["results"] as List<dynamic>);
       return ResponseDto(
           info: Info(
               count: json["info"]["count"],
@@ -17,8 +18,10 @@ class ResponseParser<T> extends BaseJsonParser<ResponseDto> {
               next: json["info"]["next"],
               prev: json["info"]["prev"]),
           results: results.map((e) => resultParser.fromJson(e)).toList());
-    } catch (e) {
-      throw Exception("Json convert exception");
+    } catch (e, stackTrace) {
+      logger.e(e);
+      logger.d(stackTrace);
+      throw Exception("Json convert exception in response parser,$e");
     }
   }
 
