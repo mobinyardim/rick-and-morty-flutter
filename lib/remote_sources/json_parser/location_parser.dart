630fc5ad65957394174f6484c1306a28a20ba646
MobinYardim <mobinyardim@gmail.com>
2023-04-24 20:59:12 +0330

    log exception

--- lib/remote_sources/json_parser/location_parser.dart
+++ lib/remote_sources/json_parser/location_parser.dart
@@ -1,15 +1,18 @@
 import 'package:rick_and_morty_flutter/remote_sources/dto/location_dto.dart';
 import 'package:rick_and_morty_flutter/remote_sources/json_parser/base_json_parser.dart';
+import '../../utils/logger.dart';
 
 class LocationParser extends BaseJsonParser<LocationDto> {
   const LocationParser() : super();
 
   @override
   LocationDto fromJson(Map<String, dynamic> json) {
-    try{
+    try {
       return LocationDto(url: json["url"], name: json["name"]);
-    }catch(e){
-      throw Exception("Json convert exception");
+    } catch (e, stackTrace) {
+      logger.e(e);
+      logger.d(stackTrace);
+      throw Exception("Json convert exception in location parser,$e");
     }
   }
 
