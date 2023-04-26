7aa8a3b2b95783e6293a7f69c297bb7d0b95336d
MobinYardim <mobinyardim@gmail.com>
2023-04-26 12:04:19 +0330

    implement character repository

---
+++ lib/repositories/character/character_repository_impl.dart
@@ -0,0 +1,18 @@
+import 'package:rick_and_morty_flutter/converters/converters.dart';
+import 'package:rick_and_morty_flutter/models/Character.dart';
+import 'package:rick_and_morty_flutter/models/result.dart';
+import 'package:rick_and_morty_flutter/repositories/character/character_repository.dart';
+
+import '../../remote_sources/character/character_remote_source.dart';
+
+class CharacterRepositoryImpl extends CharacterRepository {
+  final CharacterRemoteSource characterRemoteSource;
+
+  const CharacterRepositoryImpl({required this.characterRemoteSource});
+
+  @override
+  Future<Result<Character>> getAllCharacters(int page) async {
+    var response = await characterRemoteSource.getAll();
+    return charactersResponseConverter.toDomain(response);
+  }
+}
