1cb342aa1d6495f15b4c244a85b589203280acd3
MobinYardim <mobinyardim@gmail.com>
2023-04-26 11:50:42 +0330

    add character base response converter singleton instance

--- lib/converters/converters.dart
+++ lib/converters/converters.dart
@@ -3,6 +3,8 @@
 import 'package:rick_and_morty_flutter/converters/base_converter.dart';
 import 'package:rick_and_morty_flutter/converters/character_converter.dart';
 import 'package:rick_and_morty_flutter/converters/location_converter.dart';
+import 'package:rick_and_morty_flutter/converters/response_converter.dart';
 
 const BaseConverter locationConverter = LocationConverter();
 const BaseConverter characterConverter = CharacterConverter();
+const BaseConverter charactersResponseConverter = ResponseConverter(dataConverter: characterConverter);
