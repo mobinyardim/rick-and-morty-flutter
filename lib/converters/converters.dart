7fe93b0d4f3fb2742203343ba19893c453f37c5c
MobinYardim <mobinyardim@gmail.com>
2023-04-26 09:34:47 +0330

    add converters singletons to converters file

---
+++ lib/converters/converters.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:rick_and_morty_flutter/converters/base_converter.dart';
+import 'package:rick_and_morty_flutter/converters/character_converter.dart';
+import 'package:rick_and_morty_flutter/converters/location_converter.dart';
+
+const BaseConverter locationConverter = LocationConverter();
+const BaseConverter characterConverter = CharacterConverter();
