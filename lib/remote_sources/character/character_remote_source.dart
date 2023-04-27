6afe127fc0030f9300ffa5f39d8b9d041359b1d0
MobinYardim <mobinyardim@gmail.com>
2023-04-27 23:30:19 +0330

    move singleton logic to factory constructor

--- lib/remote_sources/character/character_remote_source.dart
+++ lib/remote_sources/character/character_remote_source.dart
@@ -1,6 +1,20 @@
+import 'package:rick_and_morty_flutter/remote_sources/character/character_remote_source_impl.dart';
 import 'package:rick_and_morty_flutter/remote_sources/dto/character_dto.dart';
 import 'package:rick_and_morty_flutter/remote_sources/dto/response_dto.dart';
 
 abstract class CharacterRemoteSource {
+  CharacterRemoteSource();
+
+  static const _instanceKey = "instance";
+  static final Map<String, CharacterRemoteSource> instances =
+      <String, CharacterRemoteSource>{};
+
+  factory CharacterRemoteSource.create() {
+    if (!instances.containsKey(_instanceKey)) {
+      instances[_instanceKey] = CharacterRemoteSourceImpl();
+    }
+    return instances[_instanceKey]!;
+  }
+
   Future<ResponseDto<CharacterDto>> getAll();
 }
--- lib/remote_sources/network.dart
+++ lib/remote_sources/network.dart
@@ -1,9 +1,5 @@
 import 'package:http/http.dart' show Client;
-import 'package:rick_and_morty_flutter/remote_sources/character/character_remote_source.dart';
-import 'package:rick_and_morty_flutter/remote_sources/character/character_remote_source_impl.dart';
 
 const baseUrl= "https://rickandmortyapi.com/api/";
 
 Client client = Client();
-
-CharacterRemoteSource characterRemoteSource = CharacterRemoteSourceImpl();
