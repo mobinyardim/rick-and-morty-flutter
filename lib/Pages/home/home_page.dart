23fd93b369e7ecdd48004c9fb62b72d9c5e5ff42
MobinYardim <mobinyardim@gmail.com>
2023-04-29 20:34:04 +0330

    get all characters from home bloc

--- lib/Pages/home/home_page.dart
+++ lib/Pages/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty_flutter/blocs/home_bloc.dart';
 import 'package:rick_and_morty_flutter/components/character_item.dart';
+import 'package:rick_and_morty_flutter/models/Character.dart';
 
 import '../../utils/character_faker.dart';
 import '../../utils/window_utils.dart';
@@ -16,15 +18,25 @@ class _HomePAgeState extends State<HomePage> {
   Widget build(BuildContext context) {
     double itemWidth = isDesktop(context) ? 400 : 200;
     double ratio = isDesktop(context) ? 2.3 : 0.56;
-
+    homeBloc.fetchAllMovies();
     return Padding(
         padding: const EdgeInsets.all(20),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: itemWidth, childAspectRatio: ratio),
-          itemBuilder: (item, index) => (Align(
-              child: CharacterItem(character: fakeNullCharacterList[index]))),
-          itemCount: fakeNullCharacterList.length,
+        child: StreamBuilder<List<Character>>(
+          stream: homeBloc.allCharacters,
+          builder: (context, AsyncSnapshot<List<Character>> snapshot) {
+            if (snapshot.hasData) {
+              return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: itemWidth, childAspectRatio: ratio),
+                  itemBuilder: (item, index) => (Align(
+                      child: CharacterItem(character: snapshot.data![index]))),
+                  itemCount: snapshot.data!.length);
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error?.toString() ?? "unknown");
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
         ));
   }
 }
