fdf1cba61e59305525fe4724d277eada2d33d8e2
MobinYardim <mobinyardim@gmail.com>
2023-04-29 20:34:45 +0330

    name refactoring

--- lib/Pages/home/home_page.dart
+++ lib/Pages/home/home_page.dart
@@ -2,18 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:rick_and_morty_flutter/blocs/home_bloc.dart';
 import 'package:rick_and_morty_flutter/components/character_item.dart';
 import 'package:rick_and_morty_flutter/models/Character.dart';
-
-import '../../utils/character_faker.dart';
 import '../../utils/window_utils.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePAgeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePAgeState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     double itemWidth = isDesktop(context) ? 400 : 200;
