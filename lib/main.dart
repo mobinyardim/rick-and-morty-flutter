c62de25cae5685198da3fdc5897cdc50094160e2
MobinYardim <mobinyardim@gmail.com>
2023-05-01 15:51:29 +0330

    use new routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:rick_and_morty_flutter/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         ),
         primarySwatch: Colors.blue,
       ),
-      routerConfig: routerConfig,
+      routerConfig: GoRouter(routes: $appRoutes),
     );
   }
 }
