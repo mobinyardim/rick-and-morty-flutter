20f5addc657b1d26ffd474b68136f007acde31a2
MobinYardim <mobinyardim@gmail.com>
2023-05-11 01:01:21 +0330

    add character list to characters page

--- lib/Pages/characters/characters_page.dart
+++ lib/Pages/characters/characters_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_flutter/blocs/home/characters_bloc.dart';
+import 'package:rick_and_morty_flutter/blocs/home/characters_state.dart';
+import 'package:rick_and_morty_flutter/components/character_item.dart';
+import 'package:rick_and_morty_flutter/components/grid_with_title.dart';
+import 'package:rick_and_morty_flutter/routes/routes.dart';
 
 class CharactersPage extends StatefulWidget {
   const CharactersPage({Key? key}) : super(key: key);
@@ -10,9 +16,25 @@ class CharactersPage extends StatefulWidget {
 class _HomePageState extends State<CharactersPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const [Text("characters page")],
-    );
+    return Padding(
+        padding: const EdgeInsets.all(20),
+        child: BlocBuilder<CharactersBloc, CharactersState>(
+          builder: (context, state) => (GridWithTitle(
+            itemsBuilder: (item, index) => (Align(
+                child: CharacterItem(
+              character: state.characters[index],
+              onPressed: () => {
+                CharacterDetailRoute(
+                        characterId: state.characters[index]?.id ?? "0")
+                    .push(context)
+              },
+            ))),
+            itemCount: state.characters.length,
+            animateHeader: false,
+            title: "Characters",
+            showSeeMoreButton: false,
+          )),
+        ));
+    ;
   }
 }
