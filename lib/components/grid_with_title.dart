b16b8c19bcc82df7068d85e5cac4724394cf34b7
MobinYardim <mobinyardim@gmail.com>
2023-05-12 23:35:57 +0330

    add controller to get on bottom reach event

--- lib/components/grid_with_title.dart
+++ lib/components/grid_with_title.dart
@@ -3,12 +3,13 @@ import 'package:rick_and_morty_flutter/components/my_sliver_persistent_header_de
 
 import '../utils/window_utils.dart';
 
-class GridWithTitle extends StatelessWidget {
+class GridWithTitle extends StatefulWidget {
   final int itemCount;
   final Widget Function(BuildContext context, int index) itemsBuilder;
   final bool animateHeader;
   final String title;
   final Function()? onSeeMore;
+  final Function()? onBottomReached;
   final bool showSeeMoreButton;
 
   const GridWithTitle(
@@ -17,9 +18,23 @@ class GridWithTitle extends StatelessWidget {
       required this.itemCount,
       required this.title,
       required this.showSeeMoreButton,
+      this.onBottomReached,
       this.onSeeMore,
       this.animateHeader = true})
       : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _GridWithTitleState();
+}
+
+class _GridWithTitleState extends State<GridWithTitle> {
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,21 +44,40 @@ class GridWithTitle extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         SliverPersistentHeader(
-          pinned: true,
+            pinned: true,
             delegate: MySliverPersistentHeaderDelegate(
-                onSeeMore: onSeeMore,
-                title: title,
-                isAnimated: animateHeader,
-                showSeeMoreButton: showSeeMoreButton)),
+                onSeeMore: widget.onSeeMore,
+                title: widget.title,
+                isAnimated: widget.animateHeader,
+                showSeeMoreButton: widget.showSeeMoreButton)),
         SliverGrid(
           delegate:
               SliverChildBuilderDelegate((BuildContext context, int index) {
-            return itemsBuilder(context, index);
-          }, childCount: itemCount),
+            return widget.itemsBuilder(context, index);
+          }, childCount: widget.itemCount),
           gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: itemWidth, childAspectRatio: ratio),
         )
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController
+      ..removeListener(_onScroll)
+      ..dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_isBottom && widget.onBottomReached != null) widget.onBottomReached!();
+  }
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) return false;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    return currentScroll >= (maxScroll * 0.9);
   }
 }
